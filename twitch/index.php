df78944065228398ba2ba80109327a73dce12e61
Gandalf1209 <gandalf1209@techpandainc.com>
2015-03-03 20:54:58 -0600

    Added Twitch Directory
    Simple redirect for shorter linkage

---
+++ twitch/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<htmL>
+	<head>
+		<title>Twitch Channel</title>
+	</head>
+	<body>
+		<h3>Redirecting to Twitch.tv</h3>
+	</body>
+	<?php 
+
+		header("Location: http://twitch.tv/gandalf1209");
+
+	?>
+</htmL>
